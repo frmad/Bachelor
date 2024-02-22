c8f18880ffd55a95d44364adaaa5c04257137f05
Sindahlof <frsin21@student.sdu.dk>
2024-02-22 11:15:26 +0100

    Created Tab navigator, App.js links directly to it, home is set to default, tab text is hidden until pressed

---
+++ CarboPlanner/views/TabNav.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from "./HomeScreen"
+import Plan from "./PlanScreen"
+import Meal from "./MealScreen"
+import Tracking from "./TrackingScreen"
+import Profile from "./ProfileScreen"
+
+export default function TabNav(){
+    const Tab = createBottomTabNavigator();
+
+    return(
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName='Home' screenOptions={({route, navigation }) => {
+        return { headerShown: false, tabBarLabel: navigation.isFocused() ? route.name : '' };
+    }}>
+                <Tab.Screen name="Home" component={Home}/>
+                <Tab.Screen name="Plan" component={Plan}/>
+                <Tab.Screen name="Meal" component={Meal}/>
+                <Tab.Screen name="Tracking" component={Tracking}/>
+                <Tab.Screen name="Profile" component={Profile}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
