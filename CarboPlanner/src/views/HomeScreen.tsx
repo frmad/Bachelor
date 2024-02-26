f84b16940067b669174674babcd650189662ea2b
Oliver Kirkegaard Svendsen <oliverksvendsen@gmail.com>
2024-02-26 16:19:42 +0100

    Add Font and Icons

--- CarboPlanner/src/views/HomeScreen.tsx
+++ CarboPlanner/src/views/HomeScreen.tsx
@@ -1,10 +1,35 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter';
 
 export default function HomeScreen(){
+  //Use Google Font (Inter)
+  let [fontsLoaded] = useFonts({
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black,
+  });
+
     return(
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        <Text style={{ fontFamily: 'Inter_700Bold', fontSize: 42}}>Home</Text>
       </View>
     );
 }
