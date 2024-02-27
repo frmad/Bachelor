244935f1c1d8d5655968b9dc70c4e663e610ab52
frmad <frmad21@student.sdu.dk>
2024-02-27 09:36:33 +0100

    Start creating the loading functionality

--- CarboPlanner/src/views/ProfileScreen.tsx
+++ CarboPlanner/src/views/ProfileScreen.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import LoadingIcon from "./LoadingIcon";
+import {useEffect, useState} from "react";
 import Loading from "./Loading";
 
+
 export default function ProfileScreen(){
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Simulate an API call - should be changed for when using the loading screen
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+    }, []);
+
+    if (isLoading) {
+        return <Loading />;
+    }
+
     return(
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Profile!</Text>
-            <Loading />
-      </View>
+            <Text>Profile!</Text>
+        </View>
     );
 }
