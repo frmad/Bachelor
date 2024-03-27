5d4488989e763fca4c46da845ec37d3b4c7808d6
frmad <frmad21@student.sdu.dk>
2024-03-27 23:59:21 +0100

    Update CalendarComponent.js

--- CarboPlanner/src/components/CalendarComponent.js
+++ CarboPlanner/src/components/CalendarComponent.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
+//https://www.npmjs.com/package/react-native-calendars#calendar
 export default function CalendarComponent() {
     const [value, onChange] = useState(new Date());
 
