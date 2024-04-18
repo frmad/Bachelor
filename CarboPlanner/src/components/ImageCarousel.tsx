f47cfcb895b6637bb8831b7febc7187a7988d31d
Oliver Kirkegaard Svendsen <oliverksvendsen@gmail.com>
2024-04-18 08:52:08 +0200

    Initial commit

--- CarboPlanner/src/components/ImageCarousel.tsx
+++ CarboPlanner/src/components/ImageCarousel.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, Image, StyleSheet, Dimensions } from 'react-native';
-import Carousel, { Pagination } from 'react-native-snap-carousel';
+import { View, Image, StyleSheet, Dimensions, SafeAreaView } from 'react-native';
+import Carousel from 'react-native-banner-carousel';
+import Carousel1, { Pagination } from 'react-native-snap-carousel';
 
 const { width } = Dimensions.get('window');
 
@@ -10,6 +11,8 @@ interface ImageCarouselProps {
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
     const [activeSlide, setActiveSlide] = useState(0);
+    const lastSlide = images.length === 0 ? activeSlide : activeSlide-1 ;
+    const nextSlide = images.length === 0 ? activeSlide : activeSlide+1 ;
 
     const renderItem = ({ item }: any) => {
         return (
@@ -19,37 +22,51 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
                     The URI 'data:image/jpeg;base64,' indicates that the image data is encoded in base64 format.
                     When the image component renders with this URI, it decodes the base64-encoded image data, which allows the image to be displayed.
                     */
+                    source={{ uri: `data:image/jpeg;base64,${images[nextSlide]}` }}
+                    style={styles.slideImage}
+                />
+                <Image
+                    /*
+                    The URI 'data:image/jpeg;base64,' indicates that the image data is encoded in base64 format.
+                    When the image component renders with this URI, it decodes the base64-encoded image data, which allows the image to be displayed.
+                    */
                     source={{ uri: `data:image/jpeg;base64,${item}` }}
+                    style={styles.slideImage}
+                />
+                <Image
+                    /*
+                    The URI 'data:image/jpeg;base64,' indicates that the image data is encoded in base64 format.
+                    When the image component renders with this URI, it decodes the base64-encoded image data, which allows the image to be displayed.
+                    */
+                    source={{ uri: `data:image/jpeg;base64,${images[lastSlide]}` }}
                     style={styles.slideImage}
                 />
             </View>
         );
     };
 
+    const renderPage = (image, index) => {
+        const first = index === 0 ? images.length-1 : index-1;
+        const last = index === images.length-1 ? 0 : index+1;
+
+        return (
+            <View key={index} style={styles.fullWidthSlide}>
+                <Image style={styles.slideImage} source={{ uri: `data:image/jpeg;base64,${images[first]}` }} />
+                <Image style={{ width: 100, height: 100 }} source={{ uri: `data:image/jpeg;base64,${image}` }} />
+                <Image style={{ width: 100, height: 100 }} source={{ uri: `data:image/jpeg;base64,${images[last]}` }} />
+            </View>
+        );
+    }
+
     const sliderWidth = width;
     const itemWidth = width;
 
     return (
-        <View style={styles.container}>
-            <Carousel
-                data={images}
-                renderItem={renderItem}
-                sliderWidth={sliderWidth}
-                itemWidth={itemWidth}
-                loop
-                onSnapToItem={(index) => setActiveSlide(index)}
-            />
-            <Pagination
-                dotsLength={images.length}
-                activeDotIndex={activeSlide}
-                containerStyle={styles.paginationContainer}
-                dotColor="#65CB2E"
-                dotStyle={styles.dotStyle}
-                inactiveDotColor="#C4C4C4"
-                inactiveDotOpacity={0.4}
-                inactiveDotScale={0.6}
-            />
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Carousel>
+                {images.map((image, index) => renderPage(image, index))}
+            </Carousel>
+        </SafeAreaView>
     );
 };
 
@@ -58,19 +75,23 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        margin: 5,
+        margin: 5
     },
     fullWidthSlide: {
         width: width,
         height: 200,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingTop: 10,
+        flex: 1,
+        flexDirection: "row"
     },
     slideImage: {
-        width: '50%',
-        height: '100%',
+        width: '45%',
+        height: '45%',
         resizeMode: 'contain',
         marginTop: 50,
+        borderRadius: 50
     },
     paginationContainer: {
         paddingVertical: 8,
