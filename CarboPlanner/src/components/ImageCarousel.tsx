cc550b986c0281d92f78f55b72d392511b3fc57f
Oliver Kirkegaard Svendsen <oliverksvendsen@gmail.com>
2024-04-19 22:28:35 +0200

    Finish design of Image Carousel

--- CarboPlanner/src/components/ImageCarousel.tsx
+++ CarboPlanner/src/components/ImageCarousel.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Image, StyleSheet, Dimensions, SafeAreaView } from 'react-native';
 import Carousel from 'react-native-banner-carousel';
-import Carousel1, { Pagination } from 'react-native-snap-carousel';
 
 const { width } = Dimensions.get('window');
 
@@ -10,40 +9,9 @@ interface ImageCarouselProps {
 }
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
-    const [activeSlide, setActiveSlide] = useState(0);
+    const [ activeSlide ] = useState(0);
     const lastSlide = images.length === 0 ? activeSlide : activeSlide-1 ;
     const nextSlide = images.length === 0 ? activeSlide : activeSlide+1 ;
-
-    const renderItem = ({ item }: any) => {
-        return (
-            <View style={styles.fullWidthSlide}>
-                <Image
-                    /*
-                    The URI 'data:image/jpeg;base64,' indicates that the image data is encoded in base64 format.
-                    When the image component renders with this URI, it decodes the base64-encoded image data, which allows the image to be displayed.
-                    */
-                    source={{ uri: `data:image/jpeg;base64,${images[nextSlide]}` }}
-                    style={styles.slideImage}
-                />
-                <Image
-                    /*
-                    The URI 'data:image/jpeg;base64,' indicates that the image data is encoded in base64 format.
-                    When the image component renders with this URI, it decodes the base64-encoded image data, which allows the image to be displayed.
-                    */
-                    source={{ uri: `data:image/jpeg;base64,${item}` }}
-                    style={styles.slideImage}
-                />
-                <Image
-                    /*
-                    The URI 'data:image/jpeg;base64,' indicates that the image data is encoded in base64 format.
-                    When the image component renders with this URI, it decodes the base64-encoded image data, which allows the image to be displayed.
-                    */
-                    source={{ uri: `data:image/jpeg;base64,${images[lastSlide]}` }}
-                    style={styles.slideImage}
-                />
-            </View>
-        );
-    };
 
     const renderPage = (image, index) => {
         const first = index === 0 ? images.length-1 : index-1;
@@ -51,9 +19,9 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({ images }) => {
 
         return (
             <View key={index} style={styles.fullWidthSlide}>
-                <Image style={styles.slideImage} source={{ uri: `data:image/jpeg;base64,${images[first]}` }} />
-                <Image style={{ width: 100, height: 100 }} source={{ uri: `data:image/jpeg;base64,${image}` }} />
-                <Image style={{ width: 100, height: 100 }} source={{ uri: `data:image/jpeg;base64,${images[last]}` }} />
+                <Image style={styles.rightSlideImage} source={{ uri: `data:image/jpeg;base64,${images[first]}` }} />
+                <Image style={styles.parentImage} source={{ uri: `data:image/jpeg;base64,${image}` }} />
+                <Image style={styles.leftSlideImage} source={{ uri: `data:image/jpeg;base64,${images[last]}` }} />
             </View>
         );
     }
@@ -84,32 +52,34 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingTop: 10,
         flex: 1,
-        flexDirection: "row"
+        flexDirection: "row",
     },
-    slideImage: {
-        width: '45%',
-        height: '45%',
-        resizeMode: 'contain',
+    leftSlideImage: {
+        width: '30%',
+        marginLeft: -20,
+        overflow: "visible",
+        height: '50%',
+        resizeMode: "cover",
         marginTop: 50,
-        borderRadius: 50
+        borderRadius: 15
     },
-    paginationContainer: {
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        position: 'absolute',
-        top: 230,
-        left: 0,
-        right: 0,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row'
+    rightSlideImage: {
+        width: '30%',
+        marginRight: -20,
+        overflow: "visible",
+        height: '50%',
+        resizeMode: "cover",
+        marginTop: 50,
+        borderRadius: 15 
     },
-    dotStyle: {
-        width: 10,
-        height: 10,
-        borderRadius: 5,
-        marginHorizontal: 8,
-    }
+    parentImage: {
+        width: '42.5%',
+        height: '60%',
+        resizeMode: "cover",
+        marginTop: 50,
+        borderRadius: 15,
+        zIndex: 30
+    },
 });
 
 export default ImageCarousel;
